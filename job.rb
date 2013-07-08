b6192f0d61efd9e50899c544df5ee14dd3e4ea49
André Barbosa <albmail88@gmail.com>
2013-07-08 16:36:16 +0100

    order always returns every job with or without dependencies

--- job.rb
+++ job.rb
@@ -6,9 +6,12 @@ module Job
     end
   private
     def job_string_to_hash jobs_string
-      jobs_hash = Hash.new
+      jobs_hash = Hash.new      
+      jobs = jobs_string.split '\n'
       
-      jobs_hash[jobs_string[0]] = nil
+      jobs.each do |job|
+        jobs_hash[job[0]] = nil
+      end
 
       return jobs_hash
     end
--- job_spec.rb
+++ job_spec.rb
@@ -11,7 +11,21 @@ module Job
 
       it 'returns a string with "a" if receiving a list with the element "a" and no dependencies' do
         Order.new.execute('a =>').should eq 'a'
-      end 
+      end
+
+      it 'returns a string with every received job without dependencies' do
+        jobs_string = 'a => \nb => \nc => \n'
+        result = Order.new.execute jobs_string
+        result.split(',').length.should be 3
+        %w(a b c).each{ |j| result.include?(j).should be_true }        
+      end
+
+      it 'returns a string with every received job with dependencies' do
+        jobs_string = 'a => \nb => c\nc => \n'
+        result = Order.new.execute jobs_string
+        result.split(',').length.should be 3
+        %w(a b c).each{ |j| result.include?(j).should be_true }        
+      end
     end
   end
 
