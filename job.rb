9314e8df5ad7f581988168693f26fb6ca035caef
André Barbosa <albmail88@gmail.com>
2013-07-08 16:13:50 +0100

    order one job with no dependencies

--- job.rb
+++ job.rb
@@ -1,7 +1,16 @@
 module Job
   class Order
-    def execute jobs
-      return ''
+    def execute jobs_string
+      job_hash = job_string_to_hash jobs_string
+      return job_hash.keys.join ','
+    end
+  private
+    def job_string_to_hash jobs_string
+      jobs_hash = Hash.new
+      
+      jobs_hash[jobs_string[0]] = nil
+
+      return jobs_hash
     end
   end
 end
--- job_spec.rb
+++ job_spec.rb
@@ -8,6 +8,10 @@ module Job
       it 'returns an empty string if provided with an empty string' do
         Order.new.execute('').empty?.should be_true
       end
+
+      it 'returns a string with "a" if receiving a list with the element "a" and no dependencies' do
+        Order.new.execute('a =>').should eq 'a'
+      end 
     end
   end
 
